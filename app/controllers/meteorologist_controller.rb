04345ca4402ad0737a26e465acf2d1724b4e16a2
dallsop <david.allsop@chicagobooth.edu>
2015-07-21 09:42:47 -0500

    HW4 Meteorologist First Pass

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -17,17 +17,32 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+    # get lat & long data
+    loc_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
+    parsed_loc_data = JSON.parse(open(loc_url).read)
+      latitude = parsed_loc_data["results"][0]["geometry"]["location"]["lat"]
+      longitude = parsed_loc_data["results"][0]["geometry"]["location"]["lng"]
 
+    # set up forecast data
+    forecast_url = "https://api.forecast.io/forecast/17593b21cc409b60b31ec90b6a5d8d38/#{latitude},#{longitude}"
+    parsed_forecast_data = JSON.parse(open(forecast_url).read)
 
-    @current_temperature = "Replace this string with your answer."
+    # define new address variable using google's clean address for output to page
+    @clean_street_address = parsed_loc_data["results"][0]["formatted_address"]
 
-    @current_summary = "Replace this string with your answer."
+    #### assign weather data to variables ####
+    @current_temperature = parsed_forecast_data["currently"]["temperature"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @current_summary = parsed_forecast_data["currently"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    # flag for minutely==nilclass because dark sky forecast doesn't return minutely information for all locations
+    parsed_forecast_data["minutely"].nil? ?
+        @summary_of_next_sixty_minutes = "No information available." :
+        @summary_of_next_sixty_minutes = parsed_forecast_data["minutely"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_forecast_data["hourly"]["summary"]
+
+    @summary_of_next_several_days = parsed_forecast_data["daily"]["summary"]
 
     render("street_to_weather.html.erb")
   end
