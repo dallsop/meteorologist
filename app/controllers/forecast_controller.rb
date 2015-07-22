ae27aa627004810a8f47021027ca10bf092bfe92
dallsop <david.allsop@chicagobooth.edu>
2015-07-22 13:07:32 -0500

    HW4 Meteorology

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'json'
 
 class ForecastController < ApplicationController
   def coords_to_weather_form
@@ -16,17 +17,22 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    url = "https://api.forecast.io/forecast/17593b21cc409b60b31ec90b6a5d8d38/#{@lat},#{@lng}"
 
+    parsed_data = JSON.parse(open(url).read)
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = parsed_data["currently"]["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    # flag for minutely==nilclass because dark sky forecast doesn't return minutely information for all locations
+    parsed_data["minutely"].nil? ?
+        @summary_of_next_sixty_minutes = "No information available." :
+        @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
     render("coords_to_weather.html.erb")
   end
--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'json'
 
 class GeocodingController < ApplicationController
   def street_to_coords_form
@@ -17,11 +18,12 @@ class GeocodingController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
+    parsed_data = JSON.parse(open(url).read)
 
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
 
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("street_to_coords.html.erb")
   end
